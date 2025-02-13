7e2cce99b267b3a3d01f747dcdb170433a16df36
Ravi Chandran <ravi.chandran@techsavvyrc.com>
2025-02-13 18:29:55 +0530

    Delete kubernetes-dashboard-token.sh

--- kubernetes-dashboard-token.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Check if the script is run by the correct user
-if [[ "$(whoami)" != "muser" ]]; then
-  echo "Error: This script must be run as user 'muser'." >&2
-  exit 1
-fi
-
-kubectl -n kubernetes-dashboard create token dashboard-admin-sa --duration=48h
